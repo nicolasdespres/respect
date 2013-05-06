412dbe2632e70476927b1d487b3a651fefb435c7
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-06 20:03:55 +0200

    Document HasConstraints.

--- lib/respect/has_constraints.rb
+++ lib/respect/has_constraints.rb
@@ -1,6 +1,14 @@
 module Respect
+  # Module supporting execution of validators referred in options.
+  #
+  # Classes including this module must fulfill the following requirements:
+  # * Respond to +options+ and returned a hash of options where keys
+  #   refers to validator name (i.e. +greater_than+ for {GreaterThanValidator}).
+  # * Respond to +validate_type(doc)+ which must returns the sanitized doc.
   module HasConstraints
 
+    # Validate all the constraints listed in +options+ to the
+    # given +value+.
     def validate_constraints(value)
       options.each do |option, arg|
         if validator_class = Respect.validator_for(option)
@@ -9,6 +17,8 @@ module Respect
       end
     end
 
+    # Call +validate_type+ with the given +doc+, apply the constraints
+    # and assign the sanitized document.
     def validate(doc)
       sanitized_doc = validate_type(doc)
       validate_constraints(sanitized_doc)
